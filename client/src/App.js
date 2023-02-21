d6deb021185b3ad3d80c674c14bed26714dbb4a6
aharmo3 <aharmo3@gmail.com>
2023-02-21 15:11:25 +0100

    Fixing really weird issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import HomeView from "./views/HomeView";
 import UsersView from "./views/UsersView";
 import UserView from "./views/UserView";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async () => {
+      // get the data from the api
+      const data = await fetch("/api");
+      // convert the data to json
+      const json = await data.json();
+      console.log(json);
+      // set state with the result
+      setData(json);
+    };
+
+    // call the function
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      Hello, welcome to React
+      <span>Hello, welcome to React</span>
       <nav>
         <Link to="/">Home</Link> | <Link to="/users">Users</Link>
       </nav>
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render({ title: "Express" });
+  res.send([{ title: "hi", age: 32 }]);
 });
 
 module.exports = router;
