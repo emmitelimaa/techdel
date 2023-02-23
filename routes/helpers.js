a7ed4545cc1d07c473cae51bb6d7950a83f99320
aharmo3 <aharmo3@gmail.com>
2023-02-23 14:10:32 +0100

    Updating DB Columns

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,10 +1,11 @@
 module.exports = {
   addRepo: (repo, guid) =>
-    `INSERT INTO repo(name, team_name,technology, company_id, score) VALUES ('${repo.name}','${repo.team_name}', '${repo.technology}','${guid}', null) `,
+    `INSERT INTO repo(repo_name, team_name,technology, company_id, repo_score) VALUES ('${repo.name}','${repo.team_name}', '${repo.technology}','${guid}', null) `,
   getAllRepos: () => `SELECT * from repo`,
   getRepoById: (id) => `SELECT * from repo WHERE company_id = ${id}`,
   addCompany: (name) =>
-    `INSERT INTO company(name, score) VALUES ('${name}', null)`,
-  getCompanyByName: (name) => `SELECT * from company WHERE name = '${name}'`,
+    `INSERT INTO company(company_name, company_score) VALUES ('${name}', null)`,
+  getCompanyByName: (name) =>
+    `SELECT * from company WHERE company_name = '${name}'`,
   getCompanyById: (id) => `SELECT * FROM company WHERE id = ${id}`,
 };
