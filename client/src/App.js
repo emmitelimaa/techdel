0874dc122ba2cfb569f7208b65b08742732da1bf
aharmo3 <aharmo3@gmail.com>
2023-02-26 15:08:10 +0100

    Removing Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState, createContext } from "react";
+import { useEffect, useState } from "react";
 import HomeView from "./components/HomeView";
 import CompaniesView from "./components/CompaniesView";
 import CompanyView from "./components/CompanyView";
 import Header from "./components/Header";
-import { URL, API } from "./constants";
+import { API } from "./constants";
 import "./App.scss";
-export const CompanyContext = createContext();
 function App() {
   const [data, setData] = useState([]);
 
@@ -22,14 +21,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CompanyContext.Provider value={data}>
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/companies" element={<CompaniesView companies={data} />}>
-            <Route path=":id" element={<CompanyView companies={data} />} />
-          </Route>
-        </Routes>
-      </CompanyContext.Provider>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/companies" element={<CompaniesView companies={data} />}>
+          <Route path=":id" element={<CompanyView companies={data} />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
